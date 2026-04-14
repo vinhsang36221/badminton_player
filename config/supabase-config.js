05dbc60ff34f54e6fa35270b56ab4d99140b001c
vinhsang36221 <140223922+vinhsang36221@users.noreply.github.com>
2026-04-14 11:42:04 +0000

    upload new code

---
+++ config/supabase-config.js
@@ -0,0 +1,4 @@
+window.BADMINTON_SUPABASE_CONFIG = {
+  url: 'https://rzhgyphwsfejicmcqgry.supabase.co',
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ6aGd5cGh3c2ZlamljbWNxZ3J5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYwNzkzNjUsImV4cCI6MjA5MTY1NTM2NX0.uQyiglx-UK_i0e3YP42-lY_XNjwkVPplmbAO6wWRJU0'
+};
